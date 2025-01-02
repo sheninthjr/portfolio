b3dacc59d4f59ca8f13b99b49325c96de21fa092
sheninthjr <sheninthjr23@gmail.com>
2025-01-02 17:45:14 +0530

    Migrated to postgresql

--- app/api/feedback/route.ts
+++ app/api/feedback/route.ts
@@ -1,7 +1,5 @@
+import prisma from '@/db';
 import { NextRequest, NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
 
 export async function POST(req: NextRequest) {
   try {
---
+++ prisma/migrations/20250102121306_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Feedback" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "feedback" TEXT NOT NULL,
+
+    CONSTRAINT "Feedback_pkey" PRIMARY KEY ("id")
+);
